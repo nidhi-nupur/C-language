2b01852ecf5db9f750ddb0ac49d5120df5990d17
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-01-19 00:27:19 +0530

    Added one code

---
+++ function.c
@@ -0,0 +1,131 @@
+// #include<stdio.h>
+// int sum(int a,int b)
+// {
+//     return a+b;  
+// }
+// main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",sum(a,b));
+// }
+
+
+
+// #include<stdio.h>
+// int mult(int a,int b);
+// int main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",mult(a,b));
+// }
+// {
+//     return a*b;
+// }
+
+
+
+
+
+// #include<stdio.h>
+// int sub(int a,int b)
+// {
+//     return a-b;  
+// }
+// int main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",sub(a,b));
+// }
+
+
+
+
+// #include<stdio.h>
+// int mult(int a,int b)
+// {
+//     return a*b;  
+// }
+// int main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",mult(a,b));
+// }
+
+
+
+
+
+// #include<stdio.h>
+// int div(int a,int b)
+// {
+//     return a/b;  
+// }
+// int main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",div(a,b));
+// }
+
+
+
+
+// #include<stdio.h>
+// int sum(int a,int b);
+// factorial(int x)
+// {
+//     int f=1;
+//     for(int i=1;i<=x;i++)
+//     {
+//         f=f*i;
+//     }
+//     return f;
+
+// }
+
+// main()
+// {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d\n",sum(a,b));
+//     printf("The Factorial is %d \n",factorial(a));
+//     printf("The Factorial is %d \n",factorial(b));
+// }
+// int sum(int a,int b)
+// {
+//     return a+b;
+// }
+
+
+
+// #include<stdio.h>
+// void jhgf()
+// {
+//     printf("cvhvdg\n");
+// }
+// int main()
+// {
+//     for(int i=1;i<=5;i++)
+//     jhgf();
+// }
+
+
+
+// #include<stdio.h>
+// int sum(int x)
+// {
+//     for(int i=1;i<=x;i++)
+//     printf("dhugued\n");
+// }
+// int main()
+// {
+//     int a;
+//     scanf("%d",&a);
+//     sum(a);
+// }
+
+
