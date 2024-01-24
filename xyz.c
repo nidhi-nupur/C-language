3257c1f7085d660ce6d4ff36a3831d9e75e6e623
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-01-24 17:08:18 +0530

    Update xyz

---
+++ xyz.c
@@ -0,0 +1,26 @@
+// switch (a)
+    // {
+    // case 1:
+    // {
+    //     printf("Add=%d",q+p);
+    // }
+    //     break;
+    // case 2:
+    // {
+    //     printf("Sub=%d",q-p);
+    // }
+    //     break;
+    // case 3:
+    // {
+    //     printf("Mult=%d",q*p);
+    // }
+    //     break;
+    // case 4:
+    // {
+    //     printf("Div=%d",q/p);
+    // }
+    //     break;
+    // default:
+    // printf("Enter Valid Number");
+    //     break;
+    // }
