b0e183e092d72fc5d105e1d10515d94edb2ba8e3
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-01-26 23:44:05 +0530

    Added one code

--- addtwovar.c
+++ addtwovar.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-
 main()
 {
-    int a,b,Sum;
-    printf("Enter any two numbers");
+    int a,b;
+    printf("Enter two random numbers= ");
     scanf("%d %d",&a,&b);
-    Sum=a+b;
-    printf("Sum=%d",Sum);
+    printf("Sum=%d",a+b);
 }
