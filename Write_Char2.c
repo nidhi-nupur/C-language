17597e2ef908b079643edc4cfa9b64b9e8e3a9eb
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-28 23:51:39 +0530

    Add one code

---
+++ Write_Char2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    FILE *fp=NULL;
+    char ch[60];
+    fp=fopen("First.txt","w");
+    if(fp==NULL)
+    {
+        printf("ERROR");
+        exit(1);
+    }
+    printf("Enter the String:");
+    gets(ch);
+    for(int i=0;i!=strlen(ch);i++)
+    fputc(ch[i],fp);
+    fclose(fp);
+}
