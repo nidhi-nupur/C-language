69807bdeaf186abeb482552179badaf6f1140171
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-30 16:33:21 +0530

    Add one code

---
+++ calloc()_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,*p;
+    int i;
+    printf("Enter the total no. of values you want:\n");
+    scanf("%d",&n);
+    p=(int*)calloc(n,sizeof(int));
+    printf("Enter the entries:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",(p+i));
+    }
+    printf("Entered entries are:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",*(p+i));
+    }
+    free(p);
+}
